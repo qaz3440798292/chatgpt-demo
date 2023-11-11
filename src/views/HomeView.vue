f67beb4a7b6c4b19924ad4649efdb1ec86d23b82
wenhaiwang <1971832840@qq.com>
2023-11-11 16:10:09 +0800

    Upload chatgpt demo project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,22 @@
   import { ref } from "vue";
   import OpenAI from "openai";
 
+  // 创建openai对象
   const openai = new OpenAI({ apiKey: import.meta.env.VITE_OPEN_AI_API_KEY, dangerouslyAllowBrowser: true });
 
+  // 用户输入的内容
   const message = ref("");
 
+  // 对话历史记录，上下文
   const history = ref([])
 
+  // 机器人回复
   const bot = ref({})
 
+  // 是否播放语音
   const audio = ref(false)
 
+  // 文本对话请求
   const textGPT = async () => {
     history.value.push({
       role: 'user',
@@ -26,27 +32,39 @@
     history.value.push(response.choices[0].message)
     bot.value = response.choices[0].message
 
+    // 判断语音功能是否开启
     if(audio.value){
       audioGPT()
     }
   }
 
+  // 语音链接
+  const url = ref("")
+
+  // 语音对话请求
   const audioGPT = async () => {
-    const response = await openai.audio.speech.create({
-      model: "tts-1-hd",
-      voice: "nova",
-      input: bot.value.content,
-      response_format: "mp3"
-    })
+    if (audio.value){
+      // 创建audio对象进行音频播放
+      const audio = new Audio()
 
-    console.log(response)
+      // 发送请求
+      const response = await openai.audio.speech.create({
+        model: "tts-1-hd",
+        voice: "nova",
+        input: bot.value.content,
+        response_format: "mp3"
+      })
 
-    const audio = new Audio()
+      console.log(response)
 
-    response.blob().then(result => {
-      audio.src = URL.createObjectURL(result)
-      audio.play()
-    })
+      response.blob().then(result => {
+        url.value = URL.createObjectURL(result)
+        audio.src = url.value
+        audio.play()
+      })
+    }else{
+      audio.value = confirm('你没有开启"播放语音"功能，你是否需要开启此功能？');
+    }
   }
 
   const clear = () => {
@@ -66,5 +84,6 @@
     <input type="checkbox" v-model="audio">播放语音
     <button @click="audioGPT">重新播放</button>
     <button @click="clear">清空对话</button>
+    <a target="_blank" :href="url" download="speech.mp3">下载音频</a>
   </div>
 </template>
